5e77e3217f0e8cb2a02926e3eeedfd7436856dc2
Gregory Eskridge <Gregory Eskridge@DESKTOP-BPSB63O>
2016-09-02 17:07:44 -0400

    Version 1 Multiplication Table.  Needs top row and left column.

--- MultiplicationTableEskridge/MultiplicationTableEskridge.cpp
+++ MultiplicationTableEskridge/MultiplicationTableEskridge.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+using namespace std;
 
 
 int main()
 {
+	int xAxis = 0;
+	int multiplicationResult = 0;
+
+	while (xAxis < 11)
+	{
+		int yAxis = 0;
+		while (yAxis < 11)
+		{
+			multiplicationResult = xAxis * yAxis;
+			printf("%d\t", multiplicationResult);
+			yAxis++;
+		}
+		printf("\n");
+		xAxis++;
+	}
     return 0;
 }
 
