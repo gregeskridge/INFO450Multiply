9738407fef4a1400d9a214f6dad3ff411b81c48b
Gregory Eskridge <Gregory Eskridge@DESKTOP-BPSB63O>
2016-09-03 15:20:41 -0400

    Version 2 ready to submit

--- MultiplicationTableEskridge/MultiplicationTableEskridge.cpp
+++ MultiplicationTableEskridge/MultiplicationTableEskridge.cpp
@@ -10,10 +10,33 @@ int main()
 {
 	int xAxis = 0;
 	int multiplicationResult = 0;
+	int columnHeader = 0;
+	int columnTab = 0;
+
+	printf("\t");
+	while (columnHeader < 11)
+	{
+		printf("%d*\t", columnHeader);
+		columnHeader++;
+	}
+
+	printf("\n");
+
+	printf("\t");
+	while (columnTab < 11)
+	{
+		printf("---\t");
+		columnTab++;
+	}
+	printf("\n");
 
 	while (xAxis < 11)
 	{
 		int yAxis = 0;
+		int rowHeader = 0;
+
+		printf("%d*\t", xAxis);
+
 		while (yAxis < 11)
 		{
 			multiplicationResult = xAxis * yAxis;
